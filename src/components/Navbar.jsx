cbe0824de1bffd9a979f04101ad0a05e29e9ca6f
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-19 11:51:07 +0500

    html to jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,37 @@
 const Navbar = () => {
   return (
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg class="bi me-2" width="40" height="32">
-          <use></use>
-        </svg>
-        <span class="fs-4">Simple header</span>
+    <div className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <span className="fs-4">Simple header</span>
       </a>
-
-      <ul class="nav nav-pills">
-        <li class="nav-item">
-          <a href="#" class="nav-link active" aria-current="page">
+      <ul className="nav nav-pills">
+        <li className="nav-item">
+          <a href="#" className="nav-link active" aria-current="page">
             Home
           </a>
         </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
             Features
           </a>
         </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
             Pricing
           </a>
         </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
             FAQs
           </a>
         </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
             About
           </a>
         </li>
       </ul>
-    </header>
+    </div>
   );
 };
 
