a66af839bbfc4e0a97038737c0e077dc08ec17dc
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-20 16:06:12 +0500

    add registerUserStart reducer and dispatch funcs to Register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import { Input } from "../ui";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUserStart } from "../slice/auth";
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isLoading } = useSelector(state => state.auth);
+
+  const handleSubmitRegister = e => {
+    e.preventDefault();
+    dispatch(registerUserStart());
+  };
 
   return (
     <div className="text-center">
-      <form className="w-25 m-auto">
+      <form className="w-25 m-auto" onSubmit={handleSubmitRegister}>
         <img className="mb-4" src="/images/logo.svg" alt="logo" width={140} />
         <h1 className="h3 mb-3 fw-normal">Please register</h1>
 
@@ -16,8 +25,8 @@ const Register = () => {
         <Input label="Email address" state={email} setState={setEmail} type="email" />
         <Input label="Password" state={password} setState={setPassword} type="password" />
 
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
-          Register
+        <button className="w-100 btn btn-lg btn-primary" disabled={isLoading} type="submit">
+          {isLoading ? "Loading..." : "Register"}
         </button>
       </form>
     </div>
