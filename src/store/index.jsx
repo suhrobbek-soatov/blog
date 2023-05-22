2677495bba7ae22288465790525a60ede00ad69c
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-22 23:36:49 +0500

    add articleReducer as article to store

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+// reducers
 import authReducer from "../slice/auth";
+import articleReducer from "../slice/article";
 
 export default configureStore({
-  reducer: { auth: authReducer },
+  reducer: {
+    auth: authReducer,
+    article: articleReducer,
+  },
   devTools: import.meta.env.NODE_ENV !== "production",
 });
