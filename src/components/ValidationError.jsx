ee55dda891d0edf353b397fb151bb44627b9c772
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-22 17:06:26 +0500

    add ValidationError komponent

---
+++ src/components/ValidationError.jsx
@@ -0,0 +1,31 @@
+import { useCallback } from "react";
+import { useSelector } from "react-redux";
+
+const ValidationError = () => {
+  const { error } = useSelector(state => state.auth);
+
+  const getError = useCallback(() => {
+    return (
+      !!error &&
+      Object.keys(error).map(name => {
+        const message = name;
+
+        return `${name} - ${error[name]}`;
+      })
+    );
+  }, [error]);
+
+  return (
+    !!error && (
+      <>
+        {getError().map(item => (
+          <div key={item} className="text-danger mb-1" role="alert">
+            {item}
+          </div>
+        ))}
+      </>
+    )
+  );
+};
+
+export default ValidationError;
