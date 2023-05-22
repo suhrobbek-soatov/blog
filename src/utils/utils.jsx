916ebaa59ad32b8f050a90fa44e7879bf68abbd1
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-22 22:39:10 +0500

    add getStorage and removeStorage functions to utils

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -2,6 +2,26 @@ export const setStorage = (key, value) => {
   try {
     localStorage.setItem(key, value);
   } catch (error) {
-    console.log("something went wrong on saving data...");
+    console.log("something went wrong while saving data...");
+  }
+};
+
+export const getStorage = key => {
+  try {
+    if (!!localStorage.getItem(key)) {
+      return localStorage.getItem(key);
+    } else {
+      throw new Error("Something went wrong while getting data...");
+    }
+  } catch (error) {
+    return null;
+  }
+};
+
+export const removeStorage = key => {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.log("something went wrong");
   }
 };
