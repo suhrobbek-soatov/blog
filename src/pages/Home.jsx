2a59b5e146cfa8b40a0d92c7494b5bfab280dd8b
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-22 23:55:16 +0500

    add Articles to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,43 @@
+import { useSelector } from "react-redux";
+
 const Home = () => {
-  return <div className="container">Home</div>;
+  const { articles } = useSelector(state => state.article);
+  return (
+    <div className="album py-5">
+      <div className="container">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          {articles.map(({ slug, title, description, author }) => (
+            <div className="col" key={slug}>
+              <div className="card h-100 shadow-sm">
+                <svg className="bd-placeholder-img card-img-top" width="100%" height={225} xmlns="http://www.w3.org/2000/svg" role="img" focusable="false">
+                  <rect width="100%" height="100%" fill="#55595c" />
+                </svg>
+
+                <div className="card-body">
+                  <p className="card-text fw-bold">{title.slice(0, 75)}...</p>
+                  <p className="card-text">{description.slice(0, 120)}...</p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button type="button" className="btn btn-sm btn-outline-success">
+                        View
+                      </button>
+                      <button type="button" className="btn btn-sm btn-outline-primary">
+                        Edit
+                      </button>
+                      <button type="button" className="btn btn-sm btn-outline-danger">
+                        Delete
+                      </button>
+                    </div>
+                    <small className="text-muted text-capitalize fw-bold">{author.username}</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
