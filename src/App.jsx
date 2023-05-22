ca2bd61ba956084f9f3953656b8fe8acb7c7442a
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-22 20:39:34 +0500

    add sign in and sign up functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import { Header, Login, Main, Register } from "./components";
+import { Navigate, Route, Routes } from "react-router-dom";
+// components
+import { Header } from "./components";
+// pages
+import { Home, Login, Register } from "./pages";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const { loggedIn } = useSelector(state => state.auth);
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={loggedIn ? <Navigate to={"/"} /> : <Login />} />
+        <Route path="/register" element={loggedIn ? <Navigate to={"/"} /> : <Register />} />
       </Routes>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,40 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { loggedIn, user } = useSelector(state => state.auth);
+
   return (
     <div className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom container">
       <Link className="d-flex align-items-center me-md-auto" to={"/"}>
         <img src="/images/logo.svg" alt="logo" />
       </Link>
-      <ul className="nav nav-pills">
-        <li className="nav-item me-3">
-          <Link className="text-decoration-none text-dark" to={"/login"}>
-            Login
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="text-decoration-none text-dark" to={"/register"}>
-            Register
-          </Link>
-        </li>
+
+      <ul className="nav nav-pills d-flex align-items-center">
+        {loggedIn && (
+          <>
+            <li className="nav-item me-3 text-capitalize">
+              <span>{user.username}</span>
+            </li>
+            <li className="nav-item me-3">
+              <button className="btn btn-outline-danger">Logout</button>
+            </li>
+          </>
+        )}
+        {!loggedIn && (
+          <>
+            <li className="nav-item me-3">
+              <Link className="text-decoration-none text-dark" to={"/login"}>
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="text-decoration-none text-dark" to={"/register"}>
+                Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
