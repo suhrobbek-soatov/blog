1af373218d80ed0ae77540a55eef2da8e3c92a2a
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-24 02:38:14 +0500

    add handleSubmit async function to CreateArticle page component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,18 @@
+import { useSelector } from "react-redux";
 import Input from "./Input";
 import TextArea from "./TextArea";
 
-const Form = ({ title, setTitle, description, setDescription, body, setBody }) => {
+const Form = props => {
+  const { title, setTitle, description, setDescription, body, setBody, handleSubmit } = props;
+  const { isLoading } = useSelector(state => state.article);
   return (
     <div className="w-50 mx-auto">
-      <form>
+      <form onSubmit={handleSubmit}>
         <Input label="Title" state={title} setState={setTitle} />
         <TextArea label="Description" state={description} setState={setDescription} />
         <TextArea label="Body" state={body} setState={setBody} height={200} />
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
-          Create
+        <button className="w-100 btn btn-lg btn-primary" disabled={isLoading} type="submit">
+          {isLoading ? "Loading..." : "Create"}
         </button>
       </form>
     </div>
--- src/pages/CreateArticle.jsx
+++ src/pages/CreateArticle.jsx
@@ -1,17 +1,39 @@
 import { useState } from "react";
 import { Form } from "../components";
+import ArticleService from "../service/article";
+import { useDispatch } from "react-redux";
+import { postArticleFailure, postArticleStart, postArticleSuccess } from "../slice/article";
+import { useNavigate } from "react-router-dom";
 
 const CreateArticle = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [body, setBody] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    const article = { title, description, body };
+    dispatch(postArticleStart());
+    try {
+      const response = await ArticleService.postArticle(article);
+      dispatch(postArticleSuccess());
+      navigate("/");
+    } catch (error) {
+      dispatch(postArticleFailure(error.response.data.errors));
+      console.log(error);
+    }
+  };
+
+  const formProps = { title, setTitle, description, setDescription, body, setBody, handleSubmit };
   return (
     <>
       <div className="text-center">
         <h1 className="fs-2 mb-2">Create Article</h1>
       </div>
-      <Form title={title} desc={description} body={body} setTitle={setTitle} setDesc={setDescription} setBody={setBody} />
+      <Form {...formProps} />
     </>
   );
 };
