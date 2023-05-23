d99092094284b8a1fd788c0d8fb8582e63ca0ed9
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-23 14:51:41 +0500

    add ArticleDetail into App Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 // components
 import { Header } from "./components";
 // pages
-import { Home, Login, Register } from "./pages";
+import { ArticleDetail, Home, Login, Register } from "./pages";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import AuthService from "./service/auth";
@@ -45,9 +45,18 @@ const App = () => {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={loggedIn ? <Home /> : <Navigate to={"/login"} />} />
-        <Route path="/login" element={loggedIn ? <Navigate to={"/"} /> : <Login />} />
-        <Route path="/register" element={loggedIn ? <Navigate to={"/"} /> : <Register />} />
+        {loggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/article/:slug" element={<ArticleDetail />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </>
+        )}
+        <Route path="*" element={<Navigate to={"/"} />} />
       </Routes>
     </>
   );
