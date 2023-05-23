6ed8134e2a96e47462db6fbcb92cf7419e51572c
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-24 01:36:42 +0500

    Update Input component, remove id from input and remove htmlFor from label

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,8 @@
 const Input = ({ type = "text", label, state, setState }) => {
   return (
     <div className="form-floating mb-3">
-      <input type={type} className="form-control" id="floatingInput" placeholder={label} value={state} onChange={(e) => setState(e.target.value)} />
-      <label htmlFor="floatingInput">{label}</label>
+      <input type={type} className="form-control" placeholder={label} value={state} onChange={e => setState(e.target.value)} />
+      <label>{label}</label>
     </div>
   );
 };
