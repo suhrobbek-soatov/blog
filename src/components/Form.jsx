4dadaa4cc765e9b3bda1d06e5a061220968d5d2f
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-24 01:49:48 +0500

    Update Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,12 @@
 import Input from "./Input";
 import TextArea from "./TextArea";
 
-const Form = ({ title, setTitle, desc, setDesc, body, setBody }) => {
+const Form = ({ title, setTitle, description, setDescription, body, setBody }) => {
   return (
     <div className="w-50 mx-auto">
       <form>
         <Input label="Title" state={title} setState={setTitle} />
-        <TextArea label="Description" state={desc} setState={setDesc} />
+        <TextArea label="Description" state={description} setState={setDescription} />
         <TextArea label="Body" state={body} setState={setBody} height={200} />
         <button className="w-100 btn btn-lg btn-primary" type="submit">
           Create
