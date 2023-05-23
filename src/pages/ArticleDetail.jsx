64d5e7e17ebb68e98b15d6d2fce66d9475b3535c
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-23 15:48:46 +0500

    show details in ArticleDetail page

--- src/pages/ArticleDetail.jsx
+++ src/pages/ArticleDetail.jsx
@@ -4,13 +4,12 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import article, { getArticleDetailFailure, getArticleDetailStart, getArticleDetailSuccess } from "../slice/article";
 import { Loader } from "../components";
+import moment from "moment/moment";
 
 const ArticleDetail = () => {
   const { slug } = useParams();
   const { isLoading, articleDetail } = useSelector(state => state.article);
   const dispatch = useDispatch();
-
-  console.log(articleDetail);
 
   const getArticleDetail = async () => {
     dispatch(getArticleDetailStart());
@@ -26,7 +25,28 @@ const ArticleDetail = () => {
     getArticleDetail();
   }, [slug]);
 
-  return <div>{isLoading && <Loader />}</div>;
+  return (
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {articleDetail && (
+            <div className="container p-3">
+              <div className="py-5">
+                <h1 className="display-5 fw-bold">{articleDetail.title}</h1>
+                <p className="col-md-8 fs-4">{articleDetail.description}</p>
+              </div>
+              <p className="text-muted">
+                <span className="fw-bold">createdAt:</span> {moment(articleDetail.createdAt).format("DD MMM, YYYY")}
+              </p>
+              <p className="fw-400">{articleDetail.body}</p>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ArticleDetail;
