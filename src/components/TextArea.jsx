3619d4963eeb5b55713d7a091af25ca174d3ee70
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-24 01:49:31 +0500

    Update TextArea component

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,7 +1,7 @@
 const TextArea = ({ label, state, setState, height = 100 }) => {
   return (
     <div className="form-floating mb-3">
-      <textarea className="form-control" placeholder="{label}" style={{ minheight: `${height}px` }} value={state} onChange={e => setState(e.target.value)} />
+      <textarea className="form-control" placeholder="{label}" style={{ minHeight: `${height}px` }} value={state} onChange={e => setState(e.target.value)} />
       <label>{label}</label>
     </div>
   );
